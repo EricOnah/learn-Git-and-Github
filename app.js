1106ef0b5b57fdb53e25652f1f87398245fd167f
Eric Onah <ericdavid4u@gmail.com>
2023-04-04 09:21:45 +0100

    modified app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("This is a pull request")
+function name() {
+    alert("This is a pull request");
+ 
+    
+}
+   
+function multiply(num) {
+    return num * num;
+  }
+console.log(multiply(2));
